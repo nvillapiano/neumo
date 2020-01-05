a09b842a578b3944905e0a6f1b350a8ebde8cc70
Nicholas Villapiano <nicholas.villapiano@ibm.com>
2020-01-04 23:37:02 -0500

    Initial set-up via Svelte cli Hello World

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<main>
+	<Header name={'Nico'}></Header>
+	<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga necessitatibus nemo aliquid. Illum, a ratione fuga officia quasi, molestias similique corporis eum vero error reprehenderit eveniet veritatis magnam doloremque est.
+  </p>
+</main>
+
+<script>
+	import Header from './components/Header.svelte';
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+  <h1>
+    <h1>Hello {name}!</h1>
+  </h1>
+</header>
+
+<script>
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: #ff3e00;
+		/* text-transform: uppercase; */
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
